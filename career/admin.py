986c5c36b1ff4f3d2a827aba5db9466a9bcd0bd3
sagargosavi <sagargosavi1112@gmail.com>
2015-05-25 20:16:24 +0530

    advance search menu with state n city

--- career/admin.py
+++ career/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from career.models import Tag,Subtag,CareerInfo, College, Courses, Course_name, Facility, Faculty
+from career.models import Tag,Subtag,CareerInfo, College, Course_name, Facility, Faculty,Faq,UserProfile,State,City,Degree
 
 admin.site.register(Tag)
 admin.site.register(Subtag)
 admin.site.register(CareerInfo)
 admin.site.register(College)
-admin.site.register(Courses)
+admin.site.register(Faq)
 admin.site.register(Course_name)
 admin.site.register(Facility)
 admin.site.register(Faculty)
+admin.site.register(UserProfile)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Degree)
 
 
